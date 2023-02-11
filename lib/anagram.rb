c87e767073ad0163d2b67af13bbf9ba1f14edf5e
Student <venus.kabera@student.moringaschool.com>
2023-02-11 22:46:08 +0300

    completed

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,12 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :word
+    def initialize(word)
+        @word = word
+    end
+    def match(array)
+        array.select do |letter|
+            letter.split("").sort == word.split("").sort
+        end
+        end
+    end
